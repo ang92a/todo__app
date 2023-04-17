7975cd56a8c3d0a1d75733be7e5d7aa37724e846
ang92 <ang_92@list.ru>
2023-04-17 22:32:54 +0300

    first commit

---
+++ client/script.js
@@ -0,0 +1,146 @@
+// Форма регистрации
+
+const box = document.querySelector(".box");
+
+function createPage() {
+  // отрисовывается форма регистрации
+
+  box.innerHTML = `<div class="Myform">
+            <h1>Регистрация на сайте</h1>
+            <div class="inp">
+                <form name="regisrration">
+                    <input class="log" type="text" required value="Логин" name="login">
+                    <input class="pass" type="password" required value="Пароль" name="password">
+                    <input class="btn" type="submit" value="Login">
+                </form>
+            </div>`;
+
+  let log = box.querySelector(".log");
+  let pass = box.querySelector(".pass");
+
+  // создается обьект из логина и пароля
+
+  let user = {
+    log: log.value,
+    pass: pass.value,
+  };
+
+  let btn = box.querySelector(".btn");
+  btn.addEventListener("click", async () => {
+    // Отправляется на сервер
+
+    let response = await fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    let result = await response.json();
+
+    //Функция получает массив с сервера со списком дел и их статусом
+    // console.log(result);
+    let arr = (result.todo = []);
+    answerServer(arr);
+  });
+}
+
+// функция для отрисовки тудушника
+
+function answerServer(list) {
+  box.inner = `<h1 class="title">ToDo List</h1>
+<div class="todo">
+    <div class="form__wrapper">
+        <form class="form" action="">
+            <input class="input" type="text" placeholder="Добавить">
+            <button class="button">Добавить</button>
+        </form>
+        <div class="info">
+        </div>
+    </div>
+    <div class="delete">
+        <button class="delEnd">Удалить завершенные</button>
+        <button class="delAll">Удалить все</button>
+    </div>
+</div> `;
+
+  let info = box.querySelector(".info");
+  const input = box.querySelector(".input");
+  const button = box.querySelector(".button");
+  const form = box.querySelector(".form");
+  const delAll = box.querySelector(".delAll");
+  const delEnd = box.querySelector(".delEnd");
+
+  list.forEach((el) => {
+    info.append(createList(el));
+  });
+}
+
+//3/Отрисовываем один Todo
+function createList(obj) {
+  const listItem = document.createElement("li");
+  listItem.className = "li";
+  listItem.innerHTML = `<input id="chk" type="checkbox">
+                        <label class="label">${obj.text}</label>
+                        <input type="text" class="textareaInput">
+                        <button class="liButtonEdit">✏️</button>
+                        <button class="liButtonOk">✅</button>
+                        <button class="liButtonDel">❌</button>`;
+  let liButtonOk = listItem.querySelector(".liButtonOk");
+  let label = listItem.querySelector(".label");
+  let textareaInput = listItem.querySelector(".textareaInput");
+  let lbl = listItem.querySelector(".liButtonEdit");
+  let chk = listItem.querySelector("#chk"); // Добавляем событие для чек
+  let liButtonDel = listItem.querySelector(".liButtonDel");
+  chk.checked = obj.status;
+  chk.addEventListener("change", () => taggleCheck(obj.id)); // для того, чтобы обойти функцию событие, создаем анонимную функцию;
+  // if (obj.status == true) lbl.classList.toggle("checkLabel"); // зачеркиваем текс
+  liButtonDel.addEventListener("click", () => taggleDel(obj.id));
+
+  lbl.addEventListener("click", () => {
+    label.style.display = "none";
+    lbl.style.display = "none";
+    liButtonOk.style.display = "block";
+    textareaInput.style.display = "block";
+    textareaInput.value = obj.text;
+    liButtonOk.addEventListener("click", () => {
+      obj.text = textareaInput.value;
+      renderList();
+    });
+  });
+  return listItem;
+}
+
+// 4/ Меняем чек при нажатии
+function taggleCheck(id) {
+  const findItem = list.find((el) => el.id === id);
+  if (findItem) findItem.status = !findItem.status;
+  renderList();
+}
+
+//5/ Удалить все
+// const deleteList = () => {
+//   // При нажатии на кнопку делит массив обнуляется и вызывается функция
+//   list = [];
+//   renderList();
+// };
+
+// // 6/ Удалить при нажатии на крестик
+function taggleDel(id) {
+  let myIndex = list.findIndex((el) => el.id === id);
+  list.splice(myIndex, 1);
+  renderList();
+}
+
+//7/ Удалить зачеркнутые
+// const deleteEnd = () => {
+//   list = list.filter((el) => el.status !== true);
+//   renderList();
+// };
+
+createPage();
+
+// delEnd.addEventListener("click", deleteEnd);
+// button.addEventListener("click", addlist);
+// delAll.addEventListener("click", deleteList);
+document.forms[0].addEventListener("submit", (e) => e.preventDefault());
---
+++ server/app.js
@@ -0,0 +1,38 @@
+// Структура сервера
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+//наша база
+const users = [];
+
+app.use(cors());
+app.use(express.json());
+
+//обработчик запроса
+app.post("/user", (req, res) => {
+  const { login, password } = req.body;
+  console.log(users);
+  let user = users.find((user) => user.login === login);
+
+  if (user) {
+    if (user.password === password) {
+      res.json({
+        message: "вход",
+        todo: user.todo,
+      });
+    } else {
+      res.status(401).json({ mes: "Неверный пароль" });
+    }
+  } else {
+    users.push({ login, password });
+    res.status(201).json({ mes: "Создан" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server has been started on port 5000...");
+});
